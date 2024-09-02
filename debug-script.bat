fcd07bcdae479e3016df0d841ebe389d1698c8cd
DinhAnh <nguyendinhanhvlqt@gmail.com>
2024-09-02 12:24:17 +0700

    update Tye and docker-compose.override.yml

--- debug-script.bat
+++ debug-script.bat
@@ -3,11 +3,12 @@ cd /d %~dp0
 
 if "%1"=="" (
     echo Rebuilding all images...
-    docker-compose down
-    docker-compose build
-    docker-compose up -d
+    docker-compose -f docker-compose.yml -f docker-compose.override.yml down 
+    docker-compose -f docker-compose.yml -f docker-compose.override.yml build 
+    docker-compose -f docker-compose.yml -f docker-compose.override.yml up -d
 ) else (
     echo Rebuilding image for service: %1
-    docker-compose build %1
-    docker-compose up -d %1
+    docker-compose -f docker-compose.yml -f docker-compose.override.yml down %1
+    docker-compose -f docker-compose.yml -f docker-compose.override.yml build %1
+    docker-compose -f docker-compose.yml -f docker-compose.override.yml up -d %1
 )
