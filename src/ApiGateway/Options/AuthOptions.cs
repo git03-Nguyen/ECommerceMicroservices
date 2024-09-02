b2fababd9a536978bb9e74529124deb0fd6d2f7d
DinhAnh <nguyendinhanhvlqt@gmail.com>
2024-09-03 00:24:23 +0700

    jkljj

--- src/ApiGateway/Options/AuthOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace ApiGateway.Options;
-
-public class AuthOptions
-{
-    public const string Name = "Authentication";
-    public string ProviderKey { get; set; } = string.Empty;
-    public string Authority { get; set; } = string.Empty;
-}
--- src/ApiGateway/Program.cs
+++ src/ApiGateway/Program.cs
@@ -1,4 +1,7 @@
-using ApiGateway.Extensions;
+
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 namespace ApiGateway;
 
@@ -9,22 +12,51 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
         
         // Add services to the container.
+        var services = builder.Services;
+        var configuration = builder.Configuration;
 
-        builder.Services
-            .ConfigureAuthentication(builder.Configuration)
-            .ConfigureOcelot(builder.Configuration, builder.Environment)
-            .ConfigureControllers()
-            .ConfigureSwaggerSupport(builder.Configuration, builder.Environment);
+        // Authentication
+        // var authConfiguration = configuration.GetSection(AuthOptions.Name).Get<AuthOptions>();
+        // var authenticationProviderKey = authConfiguration?.ProviderKey;
+        // var authority = authConfiguration?.Authority;
+        
+        var authenticationProviderKey = "IdentityApiKey";
+        var authority = "http://localhost:6100";
 
+        services.AddAuthentication()
+            .AddJwtBearer(authenticationProviderKey, options =>
+            {
+                options.Authority = authority;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = false
+                };
+            });
+            
+        
+        // Ocelot
+        builder.Services.AddOcelot(builder.Configuration);
+        builder.Configuration.AddJsonFile("ocelot.json");
+        
+        // builder.Services
+        //     .ConfigureAuthentication(builder.Configuration)
+        //     .ConfigureOcelot(builder.Configuration, builder.Environment)
+        //     .ConfigureControllers()
+        //     .ConfigureSwaggerSupport(builder.Configuration, builder.Environment);
+
+        
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-
+        app.UseRouting();
         app.UseAuthentication();
         app.UseAuthorization();
+        app.UseEndpoints(_ => { });
 
-        app.MapControllers();
-        app.UseOcelotService(builder.Environment);
+        // app.MapControllers();
+        // app.UseOcelotService(builder.Environment);
+        app.UseOcelot();
 
         app.Run();
     }
