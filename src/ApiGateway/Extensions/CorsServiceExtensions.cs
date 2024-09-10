f5e3351718370d52166940193943e7f8b62c1195
DinhAnh <nguyendinhanhvlqt@gmail.com>
2024-09-11 00:22:25 +0700

    Update DownStreamTemplate to api/v1/{everything}

--- src/ApiGateway/Extensions/CorsServiceExtensions.cs
+++ src/ApiGateway/Extensions/CorsServiceExtensions.cs
@@ -4,7 +4,23 @@ public static class CorsServiceExtensions
 {
     public static IServiceCollection ConfigureCors(this IServiceCollection services, IConfiguration configuration)
     {
-        // TODO: Configure CORS
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder =>
+            {
+                builder
+                    .WithOrigins("http://localhost:5173")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials();
+            });
+        });
         return services;
+    }
+    
+    public static IApplicationBuilder UseCorsService(this IApplicationBuilder app)
+    {
+        app.UseCors("CorsPolicy");
+        return app;
     }
 }
